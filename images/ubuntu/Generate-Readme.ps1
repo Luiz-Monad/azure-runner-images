908d8bad957e7deb574982218dd8d8067bb4afa4
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2024-05-29 08:40:25 -0500

    [skip ci] updating readme.md

--- images/ubuntu/Generate-Readme.ps1
+++ images/ubuntu/Generate-Readme.ps1
@@ -1,4 +1,10 @@
 using module ..\..\helpers\software-report-base\SoftwareReport.psm1
-$json = Get-Content -Path .\Ubuntu2204-Report.json -Raw
-$report = [SoftwareReport]::FromJson($json)
-$report.ToMarkdown() | Out-File .\Ubuntu2204-Readme.md
+
+function gen ($ver) {
+    $json = Get-Content -Path .\Ubuntu$ver-Report.json -Raw
+    $report = [SoftwareReport]::FromJson($json)
+    $report.ToMarkdown() | Out-File .\Ubuntu$ver-Readme.md
+}
+
+gen 2204
+gen 2404
